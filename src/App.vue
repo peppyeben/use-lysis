3cfda705a7ac43c017c3615a6591ed0570985827
peppyeben <austineben34@gmail.com>
2024-08-30 09:20:47 +0100

    Add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
+    <NavigationBar />
     <router-view />
 </template>
+
+<script>
+import NavigationBar from "./components/NavigationBar.vue";
+
+export default {
+    components: {
+        NavigationBar,
+    },
+};
+</script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav
+        class="relative flex w-full flex-wrap items-center justify-between bg-transparent py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4"
+    >
+        <div class="flex w-full flex-wrap items-center justify-between px-3">
+            <div>
+                <a class="mx-2 my-1 flex items-center lg:mb-0 lg:mt-0" href="#">
+                    <img
+                        src="favicon.svg"
+                        alt="TE Logo"
+                        loading="lazy"
+                        class="w-[2.5rem] rounded-full"
+                    />
+                </a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavigationBar",
+};
+</script>
